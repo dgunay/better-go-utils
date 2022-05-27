b9f13a78a2bcf1c9a7ca23502e63f5df1feec1bf
Devin Gunay <devingunay@gmail.com>
2022-05-26 22:35:15 -0700

    add Bool type

---
+++ option/bool.go
@@ -0,0 +1,28 @@
+package option
+
+type Bool bool
+
+// Can't implement this yet because of generics limitations
+func (b Bool) ThenSomeAny(thing any) Option[any] {
+	if b {
+		return Some(thing)
+	}
+
+	return None[any]()
+}
+
+func (b Bool) Then(f func() Bool) Bool {
+	if b {
+		return f()
+	}
+
+	return b
+}
+
+func (b Bool) Else(f func() Bool) Bool {
+	if !b {
+		return f()
+	}
+
+	return b
+}
